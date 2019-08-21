29fbc4c66a3796e9086d19ce2dd4ea8f0168112a
willi <willi.kappler@uni-tuebingen.de>
2019-08-21 15:19:15 +0200

    Python script for createing NetCDF files in old format

---
+++ scripts/create_files_old_format.py
@@ -0,0 +1,23 @@
+#!/bin/env python3
+
+from netCDF4 import Dataset
+
+data = Dataset("empty.nc", "w", format="NETCDF3_CLASSIC")
+data.close()
+
+data = Dataset("small1.nc", "w", format="NETCDF3_CLASSIC")
+time = data.createDimension("time", 5)
+times = data.createVariable("times","i2",("time",))
+times[:] = [1, 2, 3, 90, 321]
+data.close()
+
+data = Dataset("small2.nc", "w", format="NETCDF3_CLASSIC")
+time = data.createDimension("time", 5)
+temp = data.createDimension("temp", 5)
+times = data.createVariable("times","i2",("time",))
+times[:] = [1, 2, 3, 90, 321]
+temps = data.createVariable("temps","i2",("temp",))
+temps[:] = [30, 32, 34, 36, 40]
+data.close()
+
+
