2dc6da3ae166f7d85a5f0f474227a0adb00bfbe7
willi <willi.kappler@uni-tuebingen.de>
2019-08-23 15:21:37 +0200

    Change visibility

--- src/netcdf.rs
+++ src/netcdf.rs
@@ -10,66 +10,67 @@ use std::string::FromUtf8Error;
 // The netCDF format is described here:
 // https://www.unidata.ucar.edu/software/netcdf/docs/file_format_specifications.html
 
-pub type OneByte = [u8; 1];
-pub type TwoBytes = [u8; 2];
-pub type FourBytes = [u8; 4];
-pub type EightBytes = [u8; 8];
+pub(crate) type OneByte = [u8; 1];
+pub(crate) type TwoBytes = [u8; 2];
+pub(crate) type FourBytes = [u8; 4];
+pub(crate) type EightBytes = [u8; 8];
 
-pub const STREAMING: FourBytes = [0xff, 0xff, 0xff, 0xff];
-pub const ZERO: FourBytes = [0x00, 0x00, 0x00, 0x00];
-pub const VERSION1: FourBytes = [0x43, 0x44, 0x46, 0x01];
-pub const VERSION2: FourBytes = [0x43, 0x44, 0x46, 0x02];
-pub const VERSION4: FourBytes = [0x89, 0x48, 0x44, 0x46]; // HDF 5, TODO
+pub(crate) const STREAMING: FourBytes = [0xff, 0xff, 0xff, 0xff];
+pub(crate) const ZERO: FourBytes = [0x00, 0x00, 0x00, 0x00];
+pub(crate) const VERSION1: FourBytes = [0x43, 0x44, 0x46, 0x01];
+pub(crate) const VERSION2: FourBytes = [0x43, 0x44, 0x46, 0x02];
+pub(crate) const VERSION4: FourBytes = [0x89, 0x48, 0x44, 0x46]; // HDF 5, TODO
 
-pub const NC_DIMENSION: FourBytes = [0x00, 0x00, 0x00, 0x0a];
-pub const NC_VARIABLE: FourBytes = [0x00, 0x00, 0x00, 0x0b];
-pub const NC_ATTRIBUTE: FourBytes = [0x00, 0x00, 0x00, 0x0c];
+pub(crate) const NC_DIMENSION: FourBytes = [0x00, 0x00, 0x00, 0x0a];
+pub(crate) const NC_VARIABLE: FourBytes = [0x00, 0x00, 0x00, 0x0b];
+pub(crate) const NC_ATTRIBUTE: FourBytes = [0x00, 0x00, 0x00, 0x0c];
 
-pub const NC_BYTE: FourBytes = [0x00, 0x00, 0x00, 0x01];
-pub const NC_CHAR: FourBytes = [0x00, 0x00, 0x00, 0x02];
-pub const NC_SHORT: FourBytes = [0x00, 0x00, 0x00, 0x03];
-pub const NC_INT: FourBytes = [0x00, 0x00, 0x00, 0x04];
-pub const NC_FLOAT: FourBytes = [0x00, 0x00, 0x00, 0x05];
-pub const NC_DOUBLE: FourBytes = [0x00, 0x00, 0x00, 0x06];
+pub(crate) const NC_BYTE: FourBytes = [0x00, 0x00, 0x00, 0x01];
+pub(crate) const NC_CHAR: FourBytes = [0x00, 0x00, 0x00, 0x02];
+pub(crate) const NC_SHORT: FourBytes = [0x00, 0x00, 0x00, 0x03];
+pub(crate) const NC_INT: FourBytes = [0x00, 0x00, 0x00, 0x04];
+pub(crate) const NC_FLOAT: FourBytes = [0x00, 0x00, 0x00, 0x05];
+pub(crate) const NC_DOUBLE: FourBytes = [0x00, 0x00, 0x00, 0x06];
 
 pub struct NetCDF {
-    pub header: NetCDFHeader,
-    pub data: NetCDFData,
+    pub(crate) header: NetCDFHeader,
+    pub(crate) data: NetCDFData,
 }
 
-pub struct NetCDFHeader {
-    pub version: NetCDFVersion,
-    pub numrecs: NetCDFStreaming,
-    pub dim_list: Vec<NetCDFDimension>,
-    pub att_list: Vec<NetCDFAttribute>,
-    pub var_list: Vec<NetCDFVariable>,
+pub(crate) struct NetCDFHeader {
+    pub(crate) version: NetCDFVersion,
+    pub(crate) numrecs: NetCDFStreaming,
+    pub(crate) dim_list: Vec<NetCDFDimension>,
+    pub(crate) att_list: Vec<NetCDFAttribute>,
+    pub(crate) var_list: Vec<NetCDFVariable>,
 }
 
 impl Display for NetCDF {
     fn fmt(&self, formatter: &mut Formatter<'_>) -> Result<(), fmt::Error> {
-        write!(formatter, "Version: {:x?}\n", self.header.version)?;
-        write!(formatter, "Number of records: {:?}\n", self.header.numrecs)?;
-        write!(formatter, "Number of dimensions: {}\n", self.header.dim_list.len())?;
-        write!(formatter, "Number of attributes: {}\n", self.header.att_list.len())?;
-        write!(formatter, "Number of variables: {}\n", self.header.var_list.len())
+        let version = match self.header.version {
+            NetCDFVersion:: CDF01 => "1 (CDF01)",
+            NetCDFVersion:: CDF02 => "2 (CDF02)",
+            NetCDFVersion:: HDF5 => "4 (HDF5)",
+        };
+        write!(formatter, "Version: {}\n", version)
     }
 }
 
 #[derive(Debug)]
-pub enum NetCDFVersion {
+pub(crate) enum NetCDFVersion {
     CDF01,
     CDF02,
     HDF5,
 }
 
 #[derive(Debug)]
-pub enum NetCDFStreaming {
+pub(crate) enum NetCDFStreaming {
     Streaming,
     Normal(u32),
 }
 
 #[derive(Debug)]
-pub enum NetCDFType {
+pub(crate) enum NetCDFType {
     NCByte,
     NCChar,
     NCShort,
@@ -89,53 +90,53 @@ pub enum NetCDFValue {
 }
 
 #[derive(Debug)]
-pub struct NetCDFDimension {
-    pub name: String,
-    pub dim_length: u32,
+pub(crate) struct NetCDFDimension {
+    pub(crate) name: String,
+    pub(crate) dim_length: u32,
 }
 
 #[derive(Debug)]
-pub struct NetCDFAttribute {
-    pub name: String,
-    pub nc_type: NetCDFType,
-    pub values: Vec<NetCDFValue>,
+pub(crate) struct NetCDFAttribute {
+    pub(crate) name: String,
+    pub(crate) nc_type: NetCDFType,
+    pub(crate) values: Vec<NetCDFValue>,
 }
 
 #[derive(Debug)]
-pub struct NetCDFVariable {
-    pub name: String,
-    pub dimid: Vec<u32>,
-    pub att_list: Vec<NetCDFAttribute>,
-    pub nc_type: NetCDFType,
-    pub vsize: u32,
-    pub offset: NetCDFOffset,
+pub(crate) struct NetCDFVariable {
+    pub(crate) name: String,
+    pub(crate) dimid: Vec<u32>,
+    pub(crate) att_list: Vec<NetCDFAttribute>,
+    pub(crate) nc_type: NetCDFType,
+    pub(crate) vsize: u32,
+    pub(crate) offset: NetCDFOffset,
 }
 
 #[derive(Debug)]
-pub enum NetCDFOffset {
+pub(crate) enum NetCDFOffset {
     Pos32(u32),
     Pos64(u64),
 }
 
 #[derive(Debug)]
-pub struct NetCDFData {
-    pub non_recs: Vec<NetCDFVarData>,
-    pub recs: Vec<NetCDFRecord>,
+pub(crate) struct NetCDFData {
+    pub(crate) non_recs: Vec<NetCDFVarData>,
+    pub(crate) recs: Vec<NetCDFRecord>,
 }
 
 #[derive(Debug)]
-pub struct NetCDFVarData {
-    pub values: Vec<NetCDFValue>,
+pub(crate) struct NetCDFVarData {
+    pub(crate) values: Vec<NetCDFValue>,
 }
 
 #[derive(Debug)]
-pub struct NetCDFRecord {
-    pub record: Vec<NetCDFVarSlab>,
+pub(crate) struct NetCDFRecord {
+    pub(crate) record: Vec<NetCDFVarSlab>,
 }
 
 #[derive(Debug)]
-pub struct NetCDFVarSlab {
-    pub varslab: Vec<NetCDFValue>,
+pub(crate) struct NetCDFVarSlab {
+    pub(crate) varslab: Vec<NetCDFValue>,
 }
 
 #[derive(Debug)]
@@ -196,3 +197,24 @@ impl Display for NetCDFError {
     }
 }
 
+impl NetCDF {
+    pub fn num_of_records(&self) -> u32 {
+        match self.header.numrecs {
+            NetCDFStreaming::Streaming => 0,
+            NetCDFStreaming::Normal(n) => n,
+        }
+    }
+
+    pub fn num_of_dimensions(&self) -> u32 {
+        self.header.dim_list.len() as u32
+    }
+
+    pub fn num_of_attributes(&self) -> u32 {
+        self.header.att_list.len() as u32
+    }
+
+    pub fn num_of_variables(&self) -> u32 {
+        self.header.var_list.len() as u32
+    }
+
+}
